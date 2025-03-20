161bdf285b2193c0fa840c89cbbe811343d2477b
pedroaurelli <pedroaureliano.contato@gmail.com>
2025-03-20 10:43:10 -0300

    Remove comment tags from Google Tag Manager script in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,13 +57,11 @@ export default function RootLayout({
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              // <!-- Google Tag Manager -->
-              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-N2GPWGNB');</script>
-              // <!-- End Google Tag Manager -->
+              })(window,document,'script','dataLayer','GTM-N2GPWGNB');
             `
           }}
         />
